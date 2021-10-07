1dde608912301c0cef744ab0bd355d5bc9b56973
muhriddinsalohiddin <troynms@gmail.com>
2021-10-07 10:33:22 +0500

    Function add

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ lst = [1,2,3,4,5,6,7,8,9,10]
 
 for i in lst:
 	print(i)
+
+
 class Car:
 	def drives(self):
 		print("Mersedes Benz")
 person1.Car()
 print(person1)
+
+
+def Salom(ism):
+	print("Salom",ism)
